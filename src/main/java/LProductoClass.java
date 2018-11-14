5dd48bd726135804af1bbd83e17cd3ca6091cc3f
Andreagalera <43338979+Andreagalera@users.noreply.github.com>
2018-11-14 18:06:34 +0100

    first commit

---
+++ src/main/java/LProductoClass.java
@@ -0,0 +1,10 @@
+public class LProductoClass {
+    //Atributos
+    public int cantidad;
+    public String producto;
+
+    @Override
+    public String toString() {
+        return "Pedido [Nombre:" + producto + ", Cantidad:" + cantidad + "]";
+    }
+}
---
+++ src/main/java/UserNotFoundException.java
@@ -0,0 +1,5 @@
+//This class is going to implement the Exception. As we can see this is class is empty
+//We only need it to call in other classes like MyManager
+//We have to add the extends and then the Exception(java lang class). The Exception is an extends of its class
+public class UserNotFoundException extends Exception{
+}
